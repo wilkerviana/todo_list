2293d44e34a0dedbd6e86898e2121f31bf3a1f5c
wilkerviana <wilker.info@gmail.com>
2017-07-25 21:02:15 -0300

    Add functions open/close form

--- src/script/index.js
+++ src/script/index.js
@@ -1,6 +1,10 @@
 function initialize() {
   const cards = document.querySelectorAll('.card');
+  const btnAddCard = document.querySelector('.btn-form_activate');
+  const btnCloseForm = document.querySelector('.btn-form_close');
   cards.forEach(handleCard);
+  btnAddCard.addEventListener('click', openForm);
+  btnCloseForm.addEventListener('click', closeForm);
 }
 
 /**
@@ -10,11 +14,28 @@ function initialize() {
 function handleCard(card) {
   const remove = card.querySelector('.btn-card_remove');
   const dismiss = card.querySelector('.btn-card_dismiss');
-
+  
   remove.addEventListener('click', () => card.remove());
   dismiss.addEventListener('click', () => {
     console.log('Cancel');
   });
+}
+
+function openForm(){
+  const footerOverlay = document.querySelector('.footer-overlay');
+  const form = document.querySelector('.form-todo');
+  footerOverlay.classList.add('_active');
+  form.classList.add('_active');
+  this.classList.add('_hide');
+}
+
+function closeForm(){
+  const footerOverlay = document.querySelector('.footer-overlay');
+  const form = document.querySelector('.form-todo');
+  const btnAddCard = document.querySelector('.btn-form_activate');
+  footerOverlay.classList.remove('_active');
+  form.classList.remove('_active');
+  btnAddCard.classList.remove('_hide');
 }
 
 window.addEventListener('load', initialize);
